9692f39a98aa3b84098aa99582e23e48ccf857cf
Arnav Komaragiri <komaraac@mail.uc.edu>
2024-11-10 19:48:08 -0800

    added vineppo heuristic baseline

--- equal_info_chunking/base.py
+++ equal_info_chunking/base.py
@@ -2,64 +2,85 @@
 import torch
 
 # transformers import
-from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, PreTrainedTokenizer
+from transformers import GenerationConfig, PreTrainedModel, PreTrainedTokenizer, BatchEncoding
 from transformers.generation.utils import ModelOutput
+
+from copy import deepcopy
 
 # type checking
 from typeguard import typechecked
 from torchtyping import TensorType, patch_typeguard
-from typing import List, Dict, Union, Iterable, ForwardRef
+from typing import List, Dict, Tuple, Union, Iterable, ForwardRef
 
 # patch typeguard to use torchtyping
 patch_typeguard()
 
+# set chat messages type alias for convenience
 ChatMessages = List[Dict[str, str]]
 
-
+# set forward reference for base chunking for return type hints
 BaseChunkingRef = ForwardRef("BaseChunkingStrategy")
 # base chunking strategy with batch processing
 class BaseChunkingStrategy:
     def __init__(self, max_chunk_size: float):
         self.max_chunk_size = max_chunk_size
 
+    @typechecked
+    def parallel_infer(
+        self, model: PreTrainedModel, inputs: BatchEncoding, **kwargs
+    ) -> Tuple[TensorType["batch", "seq"], TensorType["batch", "seq", "dim"]]:
+        with torch.no_grad():
+            outputs: torch.Tensor = model(**inputs, labels=inputs["input_ids"])
+        tokens = inputs["input_ids"]
+        start_logit = torch.full((outputs.logits.shape[0], 1, outputs.logits.shape[-1]), -torch.inf)
+        batch_idx = torch.arange(outputs.logits.shape[0])
+        start_logit[batch_idx, 0, tokens[batch_idx, 0]] = 1 
+        logits = torch.cat((start_logit, outputs.logits[:, :-1, :]), dim=1)
+        return tokens, logits
+    
+    @typechecked
+    def generate_infer(
+        self, model: PreTrainedModel, inputs: BatchEncoding, **kwargs
+    ) -> Tuple[TensorType["batch", "seq"], TensorType["batch", "seq", "dim"]]:
+        generation_config = self.get_gen_config()
+        outputs: ModelOutput = model.generate(
+            **inputs,
+            generation_config=generation_config,
+            **kwargs
+        )
+        tokens = outputs.sequences[:, inputs['input_ids'].shape[1]:]
+        logits = torch.stack(outputs.logits).permute(1, 0, 2)
+        return tokens, logits
+
+    @typechecked
     def calibrate(
-            self, 
-            model: AutoModelForCausalLM, 
-            tokenizer: AutoTokenizer, 
-            docs: List[Union[str, ChatMessages]]
+            self,
+            model: PreTrainedModel,
+            tokenizer: PreTrainedTokenizer,
+            docs: List[Union[str, ChatMessages]],
+            weight: float = 1
         ) -> BaseChunkingRef:
         max_chunk_size = 0
         for d in docs:
-            if isinstance(list):
+            if isinstance(d, list):
                 d = tokenizer.apply_chat_template(
                     d,
                     tokenize=False,
                     add_generation_prompt=True
                 )
             inputs = tokenizer(d, return_tensors="pt")
-            with torch.no_grad():
-                outputs: torch.Tensor = model(**inputs, labels=inputs["input_ids"])
-            tokens = inputs["input_ids"]
-            start_logit = torch.full((outputs.logits.shape[0], 1, outputs.logits.shape[-1]), -torch.inf)
-            batch_idx = torch.arange(outputs.logits.shape[0])
-            start_logit[batch_idx, 0, tokens[batch_idx, 0]] = 1 
-            logits = torch.cat((start_logit, outputs.logits[:, :-1, :]), dim=1)
+            tokens, logits = self.parallel_infer(model, inputs)
+            max_chunk_size = max(max_chunk_size, max([self.get_chunk_size(tokens[0], logits[0], i, i + 1) for i in range(tokens.shape[1])]))
             
-            log_probs = torch.log_softmax(logits, dim=-1)
-            probs = torch.exp(log_probs)
-            ent_tensor = probs * log_probs
-            ent_tensor[probs == 0] = 0
-            ent_tensor[log_probs == 0] = 0
-            entropies = -torch.sum(ent_tensor, dim=-1)
-            max_chunk_size = max(max_chunk_size, torch.max(entropies).item())
-        return max_chunk_size
+        new_strategy = deepcopy(self)
+        new_strategy.max_chunk_size = weight * max_chunk_size
+        return new_strategy
         
     @typechecked
     def get_chunk_size(
         self, 
         tokens: TensorType["seq"], logits: TensorType["seq", "dim"], 
         start_ptr: int, end_ptr: int,
-        tokenizer: PreTrainedTokenizer
     ) -> bool:
         raise NotImplementedError("chunk sizing function not implemented")
 
@@ -73,10 +94,11 @@ class BaseChunkingStrategy:
             return_dict_in_generate=True
         ) 
     
+    @typechecked
     def iterate(
             self,
-            model: AutoModelForCausalLM,
-            tokenizer: AutoTokenizer,
+            model: PreTrainedModel,
+            tokenizer: PreTrainedTokenizer,
             prompt: Union[str, ChatMessages],
             generate: bool = True,
             yield_trailing_chunk: bool = False,
@@ -90,32 +112,19 @@ class BaseChunkingStrategy:
             )
         inputs = tokenizer(prompt, return_tensors="pt")
         if generate:
-            generation_config = self.get_gen_config()
-            outputs: ModelOutput = model.generate(
-                **inputs,
-                generation_config=generation_config,
-                **kwargs
-            )
-            tokens = outputs.sequences[:, inputs['input_ids'].shape[1]:]
-            logits = torch.stack(outputs.logits).permute(1, 0, 2)
+            tokens, logits = self.generate_infer(model, inputs, **kwargs)
         else:
-            with torch.no_grad():
-                outputs: torch.Tensor = model(**inputs, labels=inputs["input_ids"])
-            tokens = inputs["input_ids"]
-            start_logit = torch.full((outputs.logits.shape[0], 1, outputs.logits.shape[-1]), -torch.inf)
-            batch_idx = torch.arange(outputs.logits.shape[0])
-            start_logit[batch_idx, 0, tokens[batch_idx, 0]] = 1 
-            logits = torch.cat((start_logit, outputs.logits[:, :-1, :]), dim=1)
+            tokens, logits = self.parallel_infer(model, inputs, **kwargs)
 
         assert tokens.shape[0] == 1, f"expected single batch dimension tokens, received dimension {tokens.shape[0]}"
         assert logits.shape[0] == 1, f"expected single batch dimension logits, received dimension {logits.shape[0]}"
 
-        # TODO: figure out how to get batch processing working
+        # don't support batch processing since the pad token handling gets overly complex
         tokens, logits = tokens[0], logits[0]
 
         if tokens.shape[0] <= 1:
             yield tokens
-        else: 
+        else:
             base, chunk_size = 0, 0
             for i in range(1, logits.shape[0]):
                 # get chunk size including current token
@@ -134,7 +143,3 @@ class BaseChunkingStrategy:
         except NotImplementedError:
             # if we don't have a sequence end method implemented don't worry about it
             pass
-
-        
-        
-
--- main.py
+++ main.py
@@ -1,9 +1,124 @@
 import torch
 import argparse
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import matplotlib.pyplot as plt
+
+from tqdm import trange
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, PreTrainedModel, PreTrainedTokenizer
+from equal_info_chunking.base import BaseChunkingStrategy, ChatMessages
 from equal_info_chunking.entropy import EntropyChunkingStrategy
 from equal_info_chunking.gzip import GZIPChunkingStrategy
+from heuristic.math_extract_steps import split_solution
+from typing import List, Union
+
+def compute_strategy_agreement(
+        model: PreTrainedModel, tokenizer: PreTrainedTokenizer, strategy: BaseChunkingStrategy, prompt: Union[str, ChatMessages],
+        generate: bool = False, max_new_tokens: int = 100, verbose: bool = False
+    ) -> float:
+    tokens, equal_inf_chunks = [], []
+    iterator = strategy.iterate(
+        model, tokenizer, prompt, generate=generate, yield_trailing_chunk=True, max_new_tokens=max_new_tokens
+    )
+    for chunk in iterator:
+        tokens += [chunk]
+        chunk_str = tokenizer.batch_decode([chunk])[0]
+        equal_inf_chunks += [chunk_str]
+        if verbose:
+            print('=' * 50)
+            print(chunk_str)
+    if verbose:
+        print('=' * 50)
+
+    # print(len("".join(equal_inf_chunks)))
+
+    collected_output = tokenizer.batch_decode([torch.concat(tokens, dim=0)])[0]
+    # print(len(collected_output))
+    heuristic_chunks: List[str] = split_solution(collected_output)
+
+    heur_visited = [0 for _ in heuristic_chunks] 
+    info_visited = [0 for _ in equal_inf_chunks]
+    for i, info_chunk in enumerate(equal_inf_chunks):
+        for j, heur_chunk in enumerate(heuristic_chunks):
+            if info_chunk in heur_chunk or heur_chunk in info_chunk:
+                if verbose:
+                    print('=' * 50)
+                    print(info_chunk)
+                    print('-' * 50)
+                    print(heur_chunk)
+                    print('=' * 50)
+                info_visited[i] = 1
+                heur_visited[j] = 1
+            # else:
+            #     if verbose:
+            #         print('=' * 50)
+            #         print("FOUND CHUNK MISMATCH")
+            #         print(info_chunk)
+            #         print('-' * 50)
+            #         print(heur_chunk)
+            #         print('=' * 50)
+
+    # heuristic_chunk_idxs = []
+    # equal_inf_chunk_idxs = []
+
+    # base = 0
+    # for chunk in heuristic_chunks:
+    #     heuristic_chunk_idxs += [(base, base + len(chunk))]
+    #     assert chunk == collected_output[base:base+len(chunk)], f"mismatched chunk:\n{chunk}\n{'-' * 50}\n{collected_output[base:base+len(chunk)]}"
+    #     base += len(chunk) + 1
+    # print(heuristic_chunk_idxs)
+
+    # base = 0
+    # for chunk in chunks:
+    #     equal_inf_chunk_idxs += [(base, base + len(chunk))]
+    #     assert chunk == collected_output[base:base+len(chunk)], f"mismatched chunk:\n{chunk}\n{'-' * 50}\n{collected_output[base:base+len(chunk)]}"
+    #     base += len(chunk) + 1
+    # print(equal_inf_chunk_idxs)
+
+    # contains = lambda a, b: (a[0] <= b[0]) and (a[1] >= b[1])
+
+    # heur_visited = [0 for _ in heuristic_chunk_idxs] 
+    # info_visited = [0 for _ in equal_inf_chunk_idxs]
+    # for i, info_chunk in enumerate(equal_inf_chunk_idxs):
+    #     for j, heur_chunk in enumerate(heuristic_chunk_idxs):
+    #         if contains(info_chunk, heur_chunk) or contains(heur_chunk, info_chunk):
+    #             if verbose:
+    #                 print('=' * 50)
+    #                 print(info_chunk)
+    #                 print('-' * 50)
+    #                 print(heur_chunk)
+    #                 print('=' * 50)
+    #             info_visited[i] = 1
+    #             heur_visited[j] = 1
+    #         else:
+    #             if verbose:
+    #                 print('=' * 50)
+    #                 print("FOUND CHUNK MISMATCH")
+    #                 print(info_chunk)
+    #                 print(chunks[i])
+    #                 print('-' * 50)
+    #                 print(heur_chunk)
+    #                 print(heuristic_chunks[j])
+    #                 print('=' * 50)
+        
+    # print(heur_visited)
+    # print(info_visited)
+    agreement = (sum(heur_visited) + sum(info_visited)) / (len(heur_visited) + len(info_visited))
+    return agreement
+
+def plot_size_vs_agreement(
+        model: PreTrainedModel, tokenizer: PreTrainedTokenizer, strategy: BaseChunkingStrategy, prompt: Union[str, ChatMessages],
+        generate: bool = False, max_new_tokens: int = 100, step: float = 0.1, label: str = ""
+    ):
+    max_chunk_size = float(strategy.max_chunk_size)
+    sizes, purities = [], []
+    num_trials = int(1 // step)
+    for n in trange(1, num_trials + 1):
+        scale = n * step
+        strategy.max_chunk_size = max_chunk_size * scale
+        agreement = compute_strategy_agreement(model, tokenizer, strategy, prompt, generate=generate, max_new_tokens=max_new_tokens)
+        sizes += [strategy.max_chunk_size]
+        purities += [agreement]
+    plt.plot(sizes, purities, label=label)
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -13,6 +128,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('-s', '--chunk-size', type=float, default=10)
     parser.add_argument('-q', '--quantize', action='store_true')
     parser.add_argument('-n', '--max-new-tokens', type=int, default=100)
+    parser.add_argument('-w', '--weight', type=float, default=1)
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -29,29 +145,32 @@ if __name__ == "__main__":
     model = AutoModelForCausalLM.from_pretrained(args.model, low_cpu_mem_usage=True, **kwargs)
     tokenizer = AutoTokenizer.from_pretrained(args.model, trust_remote_code=True, use_fast=False)
 
-    entropy_strategy = EntropyChunkingStrategy(args.chunk_size)
-    gzip_strategy = GZIPChunkingStrategy(tokenizer, args.chunk_size)
-
     prompt = [
         {"role": "system", "content": "Please reason step by step, and put your final answer within \\boxed{}."},
         {"role": "user", "content": args.prompt}
     ]
 
+    entropy_strategy = EntropyChunkingStrategy(args.chunk_size).calibrate(model, tokenizer, [prompt], weight=args.weight)
+    gzip_strategy = GZIPChunkingStrategy(tokenizer, args.chunk_size).calibrate(model, tokenizer, [prompt], weight=args.weight)
+
     print('Entropy Chunking: ' + ('=' * 50))
-    iterator = entropy_strategy.iterate(
-        model, tokenizer, prompt, generate=args.generate, yield_trailing_chunk=True, max_new_tokens=args.max_new_tokens
+    agreement = compute_strategy_agreement(
+        model, tokenizer, entropy_strategy, prompt, 
+        generate=args.generate, max_new_tokens=args.max_new_tokens, verbose=True
     )
-    for chunk in iterator:
-        print('=' * 50)
-        print(tokenizer.batch_decode([chunk])[0])
-        print('=' * 50)
+    print(f"Entropy Chunking Agreement: {100 * agreement:.5f}%")
 
     print()
     print('GZIP Chunking: ' + ('=' * 50))
-    iterator = gzip_strategy.iterate(
-        model, tokenizer, prompt, generate=args.generate, yield_trailing_chunk=True, max_new_tokens=args.max_new_tokens
+    agreement = compute_strategy_agreement(
+        model, tokenizer, gzip_strategy, prompt, 
+        generate=args.generate, max_new_tokens=args.max_new_tokens, verbose=True
     )
-    for chunk in iterator:
-        print('=' * 50)
-        print(tokenizer.batch_decode([chunk])[0])
-        print('=' * 50)
+    print(f"GZIP Chunking Agreement: {100 * agreement:.5f}%")
+
+    plot_size_vs_agreement(model, tokenizer, entropy_strategy, prompt, generate=args.generate, max_new_tokens=args.max_new_tokens, label="Entropy Chunking")
+    plot_size_vs_agreement(model, tokenizer, gzip_strategy, prompt, generate=args.generate, max_new_tokens=args.max_new_tokens, label="GZIP Chunking")
+    plt.xlabel("Max Chunk Size")
+    plt.ylabel("Chunking Agreement")
+    plt.legend()
+    plt.show()
